862f00070d3c2b0a37dbd0960dae128c50656887
Brian <salas.pardo13@gmail.com>
2024-05-10 21:35:44 -0600

    add route delete and edit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/u/{any}',[UrlShortenerController::class,'handle']);
 Route::get('/dashboard',[UrlShortenerController::class,'dashboard']);
 
 Route::post('/url/shorten',[UrlShortenerController::class,'store']);
+
+Route::delete('url/delete/{id}',[UrlShortenerController::class,'delete']);
+
+Route::put('url/edit/{url}',[UrlShortenerController::class,'edit']);

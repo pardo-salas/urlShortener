7fd37af3e10e92c98aadeb138abff2f41ef103d3
Brian <salas.pardo13@gmail.com>
2024-05-12 22:07:26 -0600

    add asset url

--- config/app.php
+++ config/app.php
@@ -122,5 +122,6 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+    
+    'asset_url' => env('ASSET_URL', 'http://localhost/public/')
 ];

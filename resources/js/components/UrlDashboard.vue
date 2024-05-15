a155b55d3485dae9b8780d2e5e65b29bfa27f059
Brian <salas.pardo13@gmail.com>
2024-05-15 14:27:05 -0600

    add link to use shortulr

--- resources/js/components/UrlDashboard.vue
+++ resources/js/components/UrlDashboard.vue
@@ -13,7 +13,7 @@
                                 </div>
                                 <div class="mx-2 d-flex align-items-center justify-content-center">
                                     <p class="border m-2 p-2 bg-secondary rounded text-light">
-                                         / 20
+                                        / 20
                                     </p>
                                     <button @click.prevent="shortenUrl()" class="btn btn-dark">
                                         Short
@@ -30,15 +30,17 @@
             <div class="col d-flex flex-wrap justify-content-center my-2">
                 <div class="col-md-5 col-12 m-2 row border rounded p-2 " v-for="link in links" :key="link.id">
                     <div class="mt-2 d-flex item-align-cener justify-content-between">
-                        <a :href="link.new_url" target="_blank" class="text-decoration-none fs-3">
+                        <a :href="link.complete_url" target="_blank" class="text-decoration-none fs-3">
                             <span class="opacity-40">/</span>
                             <span>
-                                {{ link.shorturl }}
+                                {{ link.new_url }}
                             </span>
                         </a>
                         <div class="d-flex justify-content-end">
+                            <div class="p-2">
+                                {{ link.clicks }}
+                            </div>
                             <button @click="urlSelected=link" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#confirmModal"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"/></svg></button>
-    
                             <button class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 28H88a12 12 0 0 0-12 12v36H40a12 12 0 0 0-12 12v128a12 12 0 0 0 12 12h128a12 12 0 0 0 12-12v-36h36a12 12 0 0 0 12-12V40a12 12 0 0 0-12-12m-60 176H52V100h104Zm48-48h-24V88a12 12 0 0 0-12-12h-68V52h104Z"/></svg></button>
                         </div>
                     </div>
@@ -97,7 +99,6 @@ const links = ref("");
 
 //Variables
 let url = ref("")
-let result = ref("")
 let urlSelected=ref("")
 let inputConfirm=ref("")
 
@@ -170,9 +171,9 @@ function formatDates(array){
     array.forEach((link)=>{
         const date = new Date(link.updated_at);
         const date_created = new Date(link.created_at);
+        link.complete_url =  window.location.origin + "/u/" + link.new_url
         link.updated_at = date.toDateString()
         link.created_at = date_created.toDateString()
-        link.shorturl=link.new_url.substring(link.new_url.length-7)
     })
     links.value=array
 }

c4633fc141faaeccf092852285953ae137954f38
Brian <salas.pardo13@gmail.com>
2024-05-18 23:54:51 -0600

    add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'checkSession' => \App\Http\Middleware\CheckSessionValues::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

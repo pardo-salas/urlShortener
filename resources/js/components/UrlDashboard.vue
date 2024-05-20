df951b21064194c48dd007f6c0011575cb3cde70
Brian <salas.pardo13@gmail.com>
2024-05-20 09:38:23 -0600

    Change layout dashboard

--- resources/js/components/UrlDashboard.vue
+++ resources/js/components/UrlDashboard.vue
@@ -26,31 +26,29 @@
             </div>
         </div>
         <!-- Data -->
-        <div class="row">
-            <div class="col d-flex flex-wrap justify-content-center my-2">
-                <div class="col-md-5 col-12 m-2 row border rounded p-2 " v-for="link in links" :key="link.id">
-                    <div class="mt-2 d-flex item-align-cener justify-content-between">
-                        <a :href="link.complete_url" target="_blank" class="text-decoration-none fs-3">
-                            <span class="opacity-40">/</span>
-                            <span>
-                                {{ link.new_url }}
-                            </span>
-                        </a>
-                        <div class="d-flex justify-content-end">
-                            <div class="p-2">
-                                {{ link.clicks }}
-                            </div>
-                            <button @click="urlSelected=link" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#confirmModal"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"/></svg></button>
-                            <button @click="copyURL(link.complete_url)" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 28H88a12 12 0 0 0-12 12v36H40a12 12 0 0 0-12 12v128a12 12 0 0 0 12 12h128a12 12 0 0 0 12-12v-36h36a12 12 0 0 0 12-12V40a12 12 0 0 0-12-12m-60 176H52V100h104Zm48-48h-24V88a12 12 0 0 0-12-12h-68V52h104Z"/></svg></button>
-                        </div>
+        <div class="row justify-content-between ">
+            <div class="col-lg m-2 col-12 border rounded p-3 " v-for="link in links" :key="link.id">
+                <div class="mt-2 d-flex item-align-cener justify-content-between">
+                    <a :href="link.complete_url" target="_blank" class="text-decoration-none fs-3">
+                        <span class="opacity-40">/</span>
+                        <span>
+                            {{ link.new_url }}
+                        </span>
+                    </a>
+                    <div class="d-flex justify-content-end">
+                        <span class="d-flex align-items-center mx-2">
+                            {{ link.clicks }}
+                        </span>
+                        <button @click="urlSelected=link" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#confirmModal"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"/></svg></button>
+                        <button @click="copyURL(link.complete_url)" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 28H88a12 12 0 0 0-12 12v36H40a12 12 0 0 0-12 12v128a12 12 0 0 0 12 12h128a12 12 0 0 0 12-12v-36h36a12 12 0 0 0 12-12V40a12 12 0 0 0-12-12m-60 176H52V100h104Zm48-48h-24V88a12 12 0 0 0-12-12h-68V52h104Z"/></svg></button>
                     </div>
-                    <span class="mb-2 d-inline-block text-truncate block fs-5 opacity-50">
-                        {{ link.old_url }}
-                    </span>
-                    <p class="d-flex justify-content-end">
-                        {{ link.created_at }}
-                    </p>
                 </div>
+                <span class="mb-2 d-inline-block text-truncate block fs-5 opacity-50">
+                    {{ link.old_url }}
+                </span>
+                <p class="d-flex justify-content-end">
+                    {{ link.created_at }}
+                </p>
             </div>
         </div>
     </div>
